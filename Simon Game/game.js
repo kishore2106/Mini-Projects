803858e1ca46fdb9846358ee72f8cdd0f1c86ce4
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-01-14 15:47:48 +0530

    Check which button is pressed

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -2,62 +2,53 @@
 // $("h1").css("color","blue");
 
 var buttonColours = ["red", "blue", "green", "yellow"];
-var gamePattern = "";
-var soundColour = "";
+var gamePattern = [];
+
+//3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
+var userClickedPattern = [];
+
+$(".btn").on("click", function (){
+    var userChoosenColour = $(this).attr("id");
+    $("#"+userChoosenColour).fadeOut(100).fadeIn(100);
+    // makeSound(userChoosenColour);
+    userClickedPattern.push(userChoosenColour);
+    
+    console.log(userClickedPattern);
+
+    
+});
+
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChoosenColour = buttonColours[randomNumber];
     // console.log(randomChoosenColour);
-    $("#"+randomChoosenColour).fadeOut(100).fadeIn(100);
+    $("#"+randomChoosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    switch(randomChoosenColour){
-        case "red":
-            var audio = new Audio("./sounds/red.mp3");
-            audio.play();
-            break;
-        case "blue":
-            var audio = new Audio("./sounds/blue.mp3");
-            audio.play();
-            break;
-        case "green":
-            var audio = new Audio("./sounds/green.mp3");
-            audio.play();
-            break;
-        case "yellow":
-            var audio = new Audio("./sounds/yellow.mp3");
-            audio.play();
-            break;
-        default :
-            var audio = new Audio("./sounds/wrong.mp3");
-            audio.play();
-            break;
-    }
+    // makeSound(randomChoosenColour);
 }
 
-$(".btn").on("click", function (event){
-    var colourClicked = $(this).attr("id");
-    $("#"+colourClicked).fadeOut(100).fadeIn(100);
 
-    switch(colourClicked){
-        case "red":
-            var audio = new Audio("./sounds/red.mp3");
-            audio.play();
-            break;
-        case "blue":
-            var audio = new Audio("./sounds/blue.mp3");
-            audio.play();
-            break;
-        case "green":
-            var audio = new Audio("./sounds/green.mp3");
-            audio.play();
-            break;
-        case "yellow":
-            var audio = new Audio("./sounds/yellow.mp3");
-            audio.play();
-            break;
-        default :
-            var audio = new Audio("./sounds/wrong.mp3");
-            audio.play();
-            break;
-    }
-});
+// function makeSound(colourClicked){
+//     switch(colourClicked){
+//         case "red":
+//             var audio = new Audio("./sounds/red.mp3");
+//             audio.play();
+//             break;
+//         case "blue":
+//             var audio = new Audio("./sounds/blue.mp3");
+//             audio.play();
+//             break;
+//         case "green":
+//             var audio = new Audio("./sounds/green.mp3");
+//             audio.play();
+//             break;
+//         case "yellow":
+//             var audio = new Audio("./sounds/yellow.mp3");
+//             audio.play();
+//             break;
+//         default :
+//             var audio = new Audio("./sounds/wrong.mp3");
+//             audio.play();
+//             break;
+//     }
+// }
