a282c2fe3ec90f54f7b7fa264aa83901d65af969
kishore2106 <125995784+kishore2106@users.noreply.github.com>
2025-01-14 10:54:15 +0530

    Create a New Pattern

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,2 +1,11 @@
-alert("testing");
-$("h1").css("color","blue");
+// alert("testing");
+// $("h1").css("color","blue");
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = "";
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChoosenNumber = buttonColours[randomNumber];
+    // console.log(randomChoosenNumber);
+}
